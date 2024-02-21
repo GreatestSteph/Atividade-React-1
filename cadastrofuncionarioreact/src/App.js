29b7b265c1c106ec7b04628e849db263b9b2c6c6
GreatestSteph <dimedimechan@gmail.com>
2024-02-21 07:41:53 -0300

    Adicionando alguns componentes do projeto

---
+++ cadastrofuncionarioreact/src/App.js
@@ -0,0 +1,11 @@
+import Cabecalho from "./cabecalho_componente/cabecalho";
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cadastrofuncionarioreact/src/cabecalho_componente/cabecalho.jsx
@@ -0,0 +1,10 @@
+import { Alert } from "react-bootstrap";
+
+export default function Cabecalho(){
+    return(
+        <alert variant="light" className="text-left">
+            <h1>Walletmart</h1>
+            <h5>- Funcionários</h5>
+        </alert>
+    )
+};
