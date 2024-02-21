8d7c3f60fe0b55e000fbf8c1091af88b692219a6
GreatestSteph <dimedimechan@gmail.com>
2024-02-21 11:25:26 -0300

    Projeto finalizado!

--- cadastrofuncionarioreact/src/App.js
+++ cadastrofuncionarioreact/src/App.js
@@ -1,9 +1,13 @@
 import Cabecalho from "./cabecalho_componente/cabecalho";
+import FormularioFuncionarios from "./formulario_componente/formulariofuncionario";
 
 function App() {
   return (
     <div className="App">
+      <br/>
       <Cabecalho/>
+      <br/>
+      <FormularioFuncionarios/>
     </div>
   );
 }
--- cadastrofuncionarioreact/src/cabecalho_componente/cabecalho.jsx
+++ cadastrofuncionarioreact/src/cabecalho_componente/cabecalho.jsx
@@ -1,10 +1,21 @@
-import { Alert } from "react-bootstrap";
-
 export default function Cabecalho(){
+    const estiloCabecalho = {
+        position: 'fixed',        
+        top: 0,
+        left: 0,
+        width: '100%',            
+        backgroundColor: '#000000',
+        padding: '10px',          
+        display: 'flex',          
+        justifyContent: 'space-between',
+        zIndex: 9999,
+        color: 'white',
+    };
+
     return(
-        <alert variant="light" className="text-left">
+        <alert className="text-left" style={estiloCabecalho}>
             <h1>Walletmart</h1>
-            <h5>- Funcionários</h5>
+            <br/>
         </alert>
     )
 };
